ea24f5faa91f99a339fe430789153dbf8cc239b7
unknown <williamalmeida426@gmail.com>
2026-09-19 12:20:29 -0300

    arrumando footer

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,38 +3,38 @@ import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
- 
+
   botaoDesabilitado:boolean = true;
-  login:string='';
-  senha:string='';
- 
+  login:string = '';
+  senha:string = '';
+
   validarFormulario(){
-    if(this.login.trim()!="" && this.senha.trim() != "" ) {
+    if(this.login.trim() !=='' && this.senha.trim() !==''){
       this.botaoDesabilitado = false;
-    } else {
+    }else{
       this.botaoDesabilitado = true;
     }
-   
   }
- 
+
   fazerLogin(){
- 
+    if(this.login === "admin@email.com" && this.senha==="123"){
+      alert("Bem-vindo(a) admin!");
+    }else{
+      alert("Credenciais inválidas!");
+    }
   }
- 
+
   onBotaoClicado(){
-    alert("Botão clicado");
+    alert("Confirmado!");
   }
- 
-  teclaDigitada(evento:KeyboardEvent):void{
-    alert("Usuário digitou: "+evento.key);
+
+  teclaSolta(evento:KeyboardEvent):void{
+    alert(`o usuário digitou ${evento.key}`);
   }
- 
+
 }
- 
-
-
-
